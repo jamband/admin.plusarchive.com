531c100102a89b7e46d0da31cf5f511b719ba53f
Tomoki Morita <tmsongbooks215@gmail.com>
2017-04-03 17:57:08 +0900

    Improve gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const fs = require('fs');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
 gulp.task('js', () =>
-  gulp.src(require('./assets/js/common.json'))
+  gulp.src(JSON.parse(fs.readFileSync('./assets/js/common.json')))
     .pipe($.concat('common.js'))
     .pipe($.uglify({preserveComments: 'license'}))
     .pipe(gulp.dest('web/assets'))
@@ -24,13 +25,14 @@ gulp.task('css', () =>
 );
 
 gulp.task('font', () =>
-  gulp.src(require('./assets/fonts/common.json'))
+  gulp.src(JSON.parse(fs.readFileSync('./assets/fonts/common.json')))
     .pipe(gulp.dest('web/assets'))
 );
 
 gulp.task('build', ['js', 'css', 'font']);
 
 gulp.task('default', ['build'], () => {
-  gulp.watch('assets/js/**/*.js', ['js']);
+  gulp.watch('assets/js/**/*.{js,json}', ['js']);
   gulp.watch('assets/css/**/*.scss', ['css']);
+  gulp.watch('assets/fonts/common.json', ['font']);
 });
