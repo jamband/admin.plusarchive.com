45e358d39419d84fe25e8a9fc78bfab446ec5a44
Tomoki Morita <tmsongbooks215@gmail.com>
2018-02-16 22:23:52 +0900

    Remove unnecessary meta tag

--- views/layouts/admin/main.php
+++ views/layouts/admin/main.php
@@ -23,7 +23,6 @@ use yii\helpers\Html;
 <html lang="en-US">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= h($this->title) ?></title>
