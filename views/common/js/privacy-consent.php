c62f9a10e71327f2cef6d07b66619c7f8fae2d48
Tomoki Morita <tmsongbooks215@gmail.com>
2018-05-25 19:48:21 +0900

    Add privacy policy link on privacy consent popup

--- views/common/js/privacy-consent.php
+++ views/common/js/privacy-consent.php
@@ -31,6 +31,6 @@ toastr.options = {
     });
   }
 };
-toastr.info('We use cookies to ensure that we give you the best experience on our website. If you continue to use this site we will assume that you are happy with it.');
+toastr.info('We use cookies to ensure that we give you the best experience on our website. If you continue to use this site we will assume that you are happy with <a href="https://plusarchive.com/privacy">Privacy Policy</a>.');
 JS
 );
