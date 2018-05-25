8a19f2ee5be68fa3cd465e8df770e5cd8ae23a13
Tomoki Morita <tmsongbooks215@gmail.com>
2018-05-25 19:21:44 +0900

    Switch from analytics.js to gtag.js

--- views/common/js/analytics-tracking.php
+++ views/common/js/analytics-tracking.php
@@ -13,19 +13,22 @@
  * @var yii\web\View $this
  */
 
+$this->registerJsFile('https://www.googletagmanager.com/gtag/js?id=UA-77180716-1', [
+    'async' => true,
+    'position' => yii\web\View::POS_HEAD,
+]);
+
 $this->registerJs(<<<'JS'
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-77180716-1', 'auto');ga('send', 'pageview');
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'UA-77180716-1');
 JS
 , yii\web\View::POS_HEAD);
 
 $this->registerJs(<<<'JS'
 $(document).on('pjax:end', function () {
-    ga('set', 'location', window.location.href);
-    ga('send', 'pageview');
+    gtag('config', 'UA-77180716-1', { 'page_path': window.location.pathname+window.location.search });
 });
 JS
 );
