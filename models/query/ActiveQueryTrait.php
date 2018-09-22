eb32dd8651200562532ec9f7922fc62387ac04ba
Tomoki Morita <tmsongbooks215@gmail.com>
2018-09-22 09:46:34 +0900

    Add common ActiveQuery trait file

---
+++ models/query/ActiveQueryTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the plusarchive.com
+ *
+ * (c) Tomoki Morita <tmsongbooks215@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace app\models\query;
+
+trait ActiveQueryTrait
+{
+    /**
+     * @param string $column
+     * @return self
+     */
+    public function latest(string $column = 'created_at'): self
+    {
+        return $this->orderBy([$column => SORT_DESC]);
+    }
+
+    /**
+     * @param string $column
+     * @return self
+     */
+    public function oldest(string $column = 'created_at'): self
+    {
+        return $this->orderBy([$column => SORT_ASC]);
+    }
+}
