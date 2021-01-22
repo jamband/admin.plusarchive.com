d50abd6e0814eb72cc07e58fac8bdc2f47803342
Tomoki Morita <tmsongbooks215@gmail.com>
2021-01-23 01:36:38 +0900

    Update dependencies
    and some minor fixes

--- assets/build/plugins.js
+++ assets/build/plugins.js
@@ -2,7 +2,7 @@ module.exports = {
   Compression: require('compression-webpack-plugin'),
   CssExtract: require('mini-css-extract-plugin'),
   License: require('license-webpack-plugin').LicenseWebpackPlugin,
-  Manifest: require('webpack-manifest-plugin'),
+  Manifest: require('webpack-manifest-plugin').WebpackManifestPlugin,
   OptimizeCssAssets: require('optimize-css-assets-webpack-plugin'),
   Terser: require('terser-webpack-plugin')
 }
