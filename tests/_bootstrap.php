63e7c73a2a6f458aeb3a749d6d0d60fe7b97cfaf
Tomoki Morita <tmsongbooks215@gmail.com>
2021-11-27 21:09:32 +0900

    Add some use statements

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
