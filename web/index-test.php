a6fd6c0d9a293136dda7a08c528547e8cfc82b1b
Tomoki Morita <tmsongbooks215@gmail.com>
2021-11-27 18:29:37 +0900

    Add use statement in some files

--- web/index-test.php
+++ web/index-test.php
@@ -1,5 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+use yii\web\Application;
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
@@ -10,10 +15,10 @@ if (preg_match('/\A(localhost|dev.admin.plusarchive)\z/', $_SERVER['SERVER_NAME'
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'test');
 
-    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
 }
 
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 
-(new yii\web\Application(require __DIR__.'/../config/test.php'))->run();
+(new Application(require __DIR__.'/../config/test.php'))->run();
--- web/index.php
+++ web/index.php
@@ -2,16 +2,19 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+use yii\web\Application;
+
 require __DIR__.'/../vendor/autoload.php';
 
 if (preg_match('/\A(localhost|dev.admin.plusarchive)\z/', $_SERVER['SERVER_NAME'])) {
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'dev');
 
-    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
 }
 
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 
-(new yii\web\Application(require __DIR__.'/../config/web.php'))->run();
+(new Application(require __DIR__.'/../config/web.php'))->run();
